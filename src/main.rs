a765a1676c4457f20eeed319c3b25ecb3d091eb3
Yet Siaw Yeung <syet0001@student.monash.edu>
2019-12-09 21:15:26 +1100

    Day 9

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 
-use day8::decode;
+use day9::sensor_boost;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}", decode().unwrap());
+    println!("{:?}", sensor_boost()?);
 
     Ok(())
 }
